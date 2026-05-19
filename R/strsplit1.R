b5ecf6510be32b1e878cf22f8d0777ef4f8b944a
Amanda Dong <amandaadong15@gmail.com>
2026-05-18 20:52:34 -0700

    add test

--- R/strsplit1.R
+++ R/strsplit1.R
@@ -1,3 +1,16 @@
+#' Splits string and unlists components
+#'
+#'@param x String to be split
+#'@param split character to be split by
+#'
+#'@return A vector of strings
+#'
+#'@examples
+#' (x <- "alfa,bravo,charlie,delta")
+#' strsplit1(x, split = ",")
+#'
+#'@export
+
 strsplit1 <- function(x, split) {
   strsplit(x, split = split)[[1]]
 }
